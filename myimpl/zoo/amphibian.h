30049717d9970aff231b602dd17e1ed4a99c1941
NaroArmenia <naroarmenia9@gmail.com>
2026-01-27 18:15:56 +0400

    27.01.2026

--- myimpl/zoo/amphibian.h
+++
@@ -1,9 +0,0 @@
-#include "animals.h"
-
-class Amphibian : public Animal, public swimmer, public walker {
-public:
-    Amphibian(const std::string& name, double weight);
-    void makeSound() const override;
-    void swim() const override;
-    void walk() const override;
-};
