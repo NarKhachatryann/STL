10d3fb6c82419f7738b803a36610dc5d4837b744
NaroArmenia <naroarmenia9@gmail.com>
2026-02-03 16:39:32 +0400

    03.02.2026

--- containers/Sequence/vector/main.cpp
+++ containers/Sequence/vector/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdexcept>
 #include "vector.hpp"
+#include "../../../except.hpp"
 
 int main() {
     vector<int> vec = {1, 2, 3, 4, 5};
@@ -8,10 +9,20 @@ int main() {
     std::cout << "Back element: " << vec.back() << std::endl;
 
     try {
-        std::cout << "Element at index 10: " << vec.at(10) << std::endl;
-    } catch (const std::out_of_range& e) {
-        std::cout << std::endl;
-        std::cout << "Caught exception: " << e.what() << std::endl;
+        int index = 10;
+        
+        if (index < 0 || index >= static_cast<int>(vec.size())) {
+            throw CustomException("Error: Index out of range!");
+        }
+        
+        std::cout << "Element at " << index << " is " << vec[index] << std::endl;
     }
+        catch (const CustomException& e) {
+            std::cerr << "Caught CustomException: " << e.what() << std::endl;
+        }
+        catch (const std::exception& e) {
+            std::cerr << "Caught std::exception: " << e.what() << std::endl;
+        }
+
     return 0;
 }
